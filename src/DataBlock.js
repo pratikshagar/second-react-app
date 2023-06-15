35488dd7f89000979e995856508c9e4e4e7bd53b
Pratiksha Gargate <pratikshagargate1998@gmail.com>
2023-06-15 13:10:07 +0530

    First

---
+++ src/DataBlock.js
@@ -0,0 +1,8 @@
+function DataBlock(){
+    return (<>
+        <div class="alert alert-primary">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In cumque obcaecati corrupti autem quos eos exercitationem quo rem, deserunt animi illo similique repellat numquam? Magni optio labore voluptatibus veniam commodi architecto aliquam omnis, provident pariatur harum! Impedit, explicabo unde. Dolorem quisquam quo aspernatur, ullam quasi nemo odio veniam fugit distinctio.
+        </div>
+       </>);
+}
+export default DataBlock;
